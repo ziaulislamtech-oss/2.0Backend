37bef587f85d9585d096ea0a87ddbd3c92631bd3
Zia-Ul-Islam <ziaulislam.tech@gmail.com>
2026-07-12 22:31:53 +0500

    path problem solved

--- 2ndTry/Day-20/Backend/app.js
+++ 2ndTry/Day-20/Backend/app.js
@@ -30,7 +30,7 @@ app.use("/api/user", userRouter);
 // Serve React build
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
